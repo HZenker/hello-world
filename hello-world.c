0b57ce4f17ee143cadc1b6397ffdcf1e6e19f5b3
Helgo Heinrich Zenker <h.zenker@userart.net>
2020-04-09 17:27:31 +0200

    error fix on line 1 hello-world.c + function foo

--- hello-world.c
+++ hello-world.c
@@ -1,10 +1,18 @@
-#hello-world.c starter file to teste arago work profiles
+/** hello-world.c starter file to test arago work profiles
+/*  
+*/
 
 #include <stdio.h>
 
+int ret_foo();
+
 void main() 
 {
-    printf("Hello world\n");
+    printf("Hello world %d\n",ret_foo());
     printf("last line EOF: \n");
 }
 
+int ret_foo() {
+short nR = 0xAB;
+    return (int)nR;
+}
